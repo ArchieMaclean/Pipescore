c5882da0383d461d7fc211055f419b6bb283921b
Archie <archiepiper@hotmail.com>
2019-05-27 10:30:24 +0100

    got express working, and changed templating to pug

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let app = express();
+app.set('view engine', 'pug');
+app.set('views','./templates');
+app.use(express.static('static'));
+
+app.get('/',(req,res)=>{
+	res.render('index');
+});
+
+app.listen(5000);
