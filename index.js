1a8262e5240dab3dd488b55d66bfa546481e3f60
Archie Maclean <archiepiper@hotmail.com>
2019-06-21 21:02:06 +0100

    changed port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let express = require("express");
 let app = express();
-app.set('port',(process.env.PORT || 5000));
 app.set('view engine', 'pug');
 app.set('views','./templates');
 app.use(express.static('static'));
@@ -9,4 +8,4 @@ app.get('/',(req,res)=>{
 	res.render('index');
 });
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
